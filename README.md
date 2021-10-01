52c12671a0e00ea71c21a6d551d14dd82434136f
Charan Gautam <gautamsaicharan@yahoo.com>
2021-10-01 14:38:14 -0600

    looping through pokemons + initial styling

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+# H1 
+
 An application that takes pokemons from an external API and displays them in the website. You can get more information about a certain pokemon by click on it's name 
 
+Key Features
+● Load data from an external source (API)
+● View a list of items
+● On user action (e.g., by clicking on a list item), view details for that item
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 // list of pokemons 
 
-pokemonlist = [
+pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}, 
     {name: 'Charmander', height: 0.6, types: ['fire']},
     {name: 'Squirtle', height: 0.5, types: ['water']}
 ]
+
+for(let i=0; i<pokemonList.length; i++) {
+    let name = pokemonList[i].name
+    let height = pokemonList[i].height
+    if(height > 0.6) {
+        document.write(`<p>${name} (${height} m) - Wow that's big </p>`)
+    } else {
+        document.write(`<p> ${name} (${height} m) </p>`)
+    }
+    
+}
 
 
