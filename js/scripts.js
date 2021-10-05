6e74e1516f45e5cb426c6c23e2be4ddd86a47160
Charan Gautam <gautamsaicharan@yahoo.com>
2021-10-04 22:17:26 -0600

    forEach pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,13 @@ pokemonList = [
     {name: 'Squirtle', height: 0.5, types: ['water']}
 ]
 
-for(let i=0; i<pokemonList.length; i++) {
-    let name = pokemonList[i].name
-    let height = pokemonList[i].height
+pokemonList.forEach(function (pokemon) {
+    let name = pokemon.name
+    let height = pokemon.height
     if(height > 0.6) {
         document.write(`<p>${name} (${height} m) - Wow that's big </p>`)
     } else {
         document.write(`<p> ${name} (${height} m) </p>`)
     }
-    
-}
-
+})
 
